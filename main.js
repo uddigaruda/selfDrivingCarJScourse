acb1e6dcc8f73f1199b25eb0adaba6fa2d63ea09
u <uddigaruda.uddigaruda@gmail.com>
2022-07-06 20:46:51 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const canvas=document.getElementById("myCanvas");
+
+canvas.width=600;
+
+const ctx = canvas.getContext("2d");
+const road=new Road(canvas.width/2,canvas.width*0.9);
+const car=new Car(canvas.width/2,window.innerHeight-50,30,50);
+
+animate();
+
+function animate(){
+    car.update();
+    canvas.height=window.innerHeight;
+    road.draw(ctx);
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
---
+++ road.js
@@ -0,0 +1,34 @@
+class Road{
+    constructor(x,width,laneCount=3 ){
+        this.x=x;
+        this.width=width;
+        this.laneCount=laneCount;
+
+        this.left=x-width/2;
+        this.right=x+width/2;
+
+        const infinity=1000000;
+        this.top=-infinity;
+        this.bottom=infinity;
+    }
+
+    draw(ctx){
+        ctx.lineWidth=25;
+        ctx.strokeStyle="white";
+
+        for(let i=0;i<=this.laneCount;i++){
+            const x=lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+            if(i>0 && i<this.laneCount){}
+
+            ctx.beginPath();
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(y,this.bottom);
+            ctx.stroke();
+            }
+    }
+}
+
