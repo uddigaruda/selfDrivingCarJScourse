964ad5d82f7b01e9f80958d1f609363ca2201d14
uddigaruda <61036103+uddigaruda@users.noreply.github.com>
2022-07-31 02:44:49 +0300

    nnetwork1levelInputsUndefined commit

--- car.js
+++ car.js
@@ -13,7 +13,10 @@ class Car{
         this.damaged=false;
         
         if (controlType!="DUMMY"){
-        this.sensor=new Sensor(this);
+            this.sensor=new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
         }
         this.controls=new Controls(controlType);
     }
@@ -26,6 +29,11 @@ class Car{
         }
         if(this.sensor){
             this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+            console.log(outputs);
         }
     }
 
@@ -109,11 +117,11 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx){
+    draw(ctx,color){
         if(this.damaged){
             ctx.fillStyle="gray";
         }else{
-            ctx.fillstyle="black";
+            ctx.fillStyle=color;
         }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
